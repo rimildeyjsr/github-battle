279c1d86e04b25f0b7618b2f2501ae7683f244b6
rimildeyjsr <rimildeyjsr@gmail.com>
2017-02-05 12:59:05 +0530

    created .babelrc and webpack config files with osme info

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './app/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: "index.bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"
+            }
+        ]
+    }
+}
