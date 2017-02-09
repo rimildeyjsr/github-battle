b6fca1473d592e0125f29188d027e18074ba4b5e
rimildeyjsr <rimildeyjsr@gmail.com>
2017-02-09 17:03:44 +0530

    prompt container added

---
+++ app/components/Prompt.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Prompt = React.createClass({
+    render: function(){
+        return(
+            <div>
+                prompt
+            </div>
+        )
+    }
+});
+
+module.exports = Prompt;
