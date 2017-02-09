9d915a0106c35cbda96c0da1784094d781810db4
rimildeyjsr <rimildeyjsr@gmail.com>
2017-02-09 15:09:35 +0530

    prompt container added with props for player one

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,7 +3,7 @@ var React = require('react');
 var Home = React.createClass({
     render: function(){
         return(
-            <div>Hello from the Home component!</div>
+            <div></div>
         )
     }
 });
--- app/config/routes.js
+++ app/config/routes.js
@@ -7,6 +7,7 @@ var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer');
 
 var routes = (
     <Router history={hashHistory}>
@@ -14,8 +15,6 @@ var routes = (
             <IndexRoute component={Home}/>
             <Route path="playerOne" header="Player One" component={PromptContainer}/>
             <Route path="playerTwo/:playerOne" header="Player Two" component={PromptContainer}/>
-            <Route path="battle" component={ConfirmBattleContainer}/>
-            <Route path="results" component={ResultsContainer} />
         </Route>
     </Router>
 );
