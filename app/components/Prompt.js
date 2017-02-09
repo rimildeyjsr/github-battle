36123c1b901f67222adc1d3c143eed28720ca3b0
rimildeyjsr <rimildeyjsr@gmail.com>
2017-02-09 17:06:27 +0530

    ui removed from prompt container to prompt component, but more fixing is required

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -3,8 +3,26 @@ var React = require('react');
 var Prompt = React.createClass({
     render: function(){
         return(
-            <div>
-                prompt
+            <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
+                <h1>{this.props.route.header}</h1>
+                <div className="col-sm-12">
+                    <form onSubmit={this.onSubmitUser}>
+                        <div className="form-group">
+                            <input className="form-control"
+                                   placeholder="Github Username"
+                                   type="text"
+                                   onChange={this.onUpdateUser}
+                                   value={this.state.username}
+                            />
+                        </div>
+
+                        <div className="form-group col-sm-4 col-sm-offset-4">
+                            <button className="btn btn-block btn-success" type="submit">
+                                Continue
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
