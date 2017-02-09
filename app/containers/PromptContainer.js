7409a2ce622d780365fe9c0af8444d8a6e06e31c
rimildeyjsr <rimildeyjsr@gmail.com>
2017-02-09 15:47:32 +0530

    added state to the input field

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,6 +1,19 @@
 var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 var PromptContainer = React.createClass({
+
+    getInitialState: function(){
+        return{
+           username: ''
+        }
+    },
+
+    onUpdateUser: function(e){
+        this.setState({
+            username: e.target.value
+        })
+    },
+
     render: function(){
         return(
             <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
@@ -8,7 +21,12 @@ var PromptContainer = React.createClass({
                 <div className="col-sm-12">
                     <form>
                         <div className="form-group">
-                            <input className="form-control" placeholder="Github Username" type="text"/>
+                            <input className="form-control"
+                                   placeholder="Github Username"
+                                   type="text"
+                                   onChange={this.onUpdateUser}
+                                   value={this.state.username}
+                            />
                         </div>
 
                         <div className="form-group col-sm-4 col-sm-offset-4">
