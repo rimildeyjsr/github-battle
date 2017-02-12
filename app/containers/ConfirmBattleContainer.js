deeaa197564237f26845e806884811918e8eb03c
rimildeyjsr <rimildeyjsr@gmail.com>
2017-02-12 10:49:47 +0530

    basic skeleton ready for confirm battle route

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,9 +1,21 @@
 var React = require('react');
+var ConfirmBattle = require('../Components/ConfirmBattle');
 
 var ConfirmBattleContainer = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
+    },
+
+    getInitialState: function() {
+        return {
+            isLoading: true,
+            PlayerInfo: []
+        }
+    },
+
     render: function(){
         return(
-            <div></div>
+            <ConfirmBattle />
         );
     }
 });
